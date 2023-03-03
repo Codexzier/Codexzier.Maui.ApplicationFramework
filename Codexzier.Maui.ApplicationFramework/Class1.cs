b63b419108c8094563bb378784afbb11df8cbfe1
Codexzier <johannes.paul.langner@live.de>
2023-03-03 17:49:08 +0100

    Add project files.

---
+++ Codexzier.Maui.ApplicationFramework/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Codexzier.Maui.ApplicationFramework
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ Codexzier.Maui.ApplicationFramework/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Codexzier.Maui.ApplicationFramework
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
