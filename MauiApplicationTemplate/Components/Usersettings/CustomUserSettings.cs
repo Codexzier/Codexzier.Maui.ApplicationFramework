f91de67f3a5769caeb2fb7af9385547e72013610
Codexzier <johannes.paul.langner@live.de>
2024-01-07 18:56:01 +0100

    Add settings

---
+++ MauiApplicationTemplate/Components/Usersettings/CustomUserSettings.cs
@@ -0,0 +1,8 @@
+﻿using Codexzier.Maui.ApplicationFramework.Components.UserSettings;
+
+namespace MauiApplicationTemplate.Components.Usersettings
+{
+    internal class CustomUserSettings : SettingsFile
+    {
+    }
+}
