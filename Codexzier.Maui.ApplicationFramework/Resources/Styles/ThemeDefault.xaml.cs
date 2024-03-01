a967b7e32056aedbf1dab490d633c3adf03fc17d
Codexzier <johannes.paul.langner@live.de>
2024-03-01 07:37:51 +0100

    zwischenstand

---
+++ Codexzier.Maui.ApplicationFramework/Resources/Styles/ThemeDefault.xaml.cs
@@ -0,0 +1,9 @@
+namespace Codexzier.Maui.ApplicationFramework.Resources.Styles;
+
+public partial class ThemeDefault : ResourceDictionary
+{
+	public ThemeDefault()
+	{
+		InitializeComponent();
+	}
+}
